abafedb3c05c27405021be4c4fdfa0ce87bca7b3
avirajkhare00 <avirajkhare00@gmail.com>
2025-06-09 00:03:23 +0530

    debug 4

--- server/dist/explorer/getChains.js
+++ server/dist/explorer/getChains.js
@@ -30,7 +30,6 @@ dotenv_1.default.config();
 function getChains(OktoAuthToken) {
     return __awaiter(this, void 0, void 0, function* () {
         var _a;
-        console.log("bearer: ", OktoAuthToken);
         try {
             const response = yield axios_1.default.get("https://sandbox-api.okto.tech/api/oc/v1/supported/networks", {
                 headers: {
--- server/src/explorer/getChains.ts
+++ server/src/explorer/getChains.ts
@@ -14,7 +14,6 @@ dotenv.config();
  * @throws Error if the API request fails.
  */
 export async function getChains(OktoAuthToken: string) {
-  console.log("bearer: ", OktoAuthToken);
   try {
     const response = await axios.get(
       "https://sandbox-api.okto.tech/api/oc/v1/supported/networks",
